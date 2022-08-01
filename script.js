7ac0678f9f98434c9215eee90fba21e72dec290b
mateus <mateus.p.ruppel@gmail.com>
2022-08-01 14:10:23 -0300

    fixed

--- script.js
+++ script.js
@@ -24,9 +24,9 @@ var saved_time = 0;
 var isRunning = false; 
 var Paused = false; 
 var isLogged = false; 
+//window.onload = logar;
+document.getElementById("time").innerHTML = "00:00:00";
 
-window.onload = logar;
-console.log(test)
 
 //#region aaa
 
@@ -200,8 +200,8 @@ function stop() {
     min = 0
     sec = 0
     
-    document.getElementById("time").innerHTML = "00:00:00";
- /*   if(Paused == false) document.getElementById("time-pause").innerHTML = "Tempo em Pausa : 00:00:00";*/
+ //   document.getElementById("time").innerHTML = "00:00:00";
+    /*   if(Paused == false) document.getElementById("time-pause").innerHTML = "Tempo em Pausa : 00:00:00";*/
     isRunning = false;
     console.log("log"+ isLogged + " running" + isRunning + "pause" + Paused)
     
@@ -227,36 +227,223 @@ function timer(_sec, _min, _hours){
     _sec = sec
     _min = min
     _hours = hours
-
-
+    
+    
     var format = 
     (_hours < 10 ? "0" + _hours : _hours) +
     ":" + 
     (_min < 10 ? "0" + _min : _min) +
     ":" + 
     (_sec < 10 ? "0" + _sec : _sec)
-
-
-
-
-   
-    var div=   document.getElementById("time")
+    
+    
+    
+    
+    
+    //var div=   document.getElementById("time")
     div.innerText = format
-
+    
     
 }
 
 
 
 function clear_time(){
-
+    
     sec = 0
     min = 0
     hours = 0    
-
-    var div=   document.getElementById("time")
+    
+   // var div=   document.getElementById("time")
     div.innerText= "00:00:00"
 }
 
 
 //#endregion
+
+
+var currentH , currentS , currentM
+
+
+var saveH , saveS , saveM
+
+
+var abc = false
+
+
+var temps = 0
+var tempm = 10
+var temph = 0
+function time()
+{   
+    today=new Date();
+    currentH=today.getHours();
+    currentM =today.getMinutes();
+    currentS = today.getSeconds();
+    currentS = currentS < 10 ? "0" + currentS : currentS
+    currentM = currentM< 10 ? "0" + currentM : currentM
+    
+    document.getElementById('txt').innerHTML=currentH+":"+currentM +":"+currentS;
+    
+    
+    if(abc == true){
+        a();        
+    }
+    setTimeout('time()',500);
+
+    abc = true;
+    
+}
+
+function desligou(){
+    alert('AAAAAAAAAAAAAAAAAA')
+}
+
+function TTC (){
+    var ttc = currentS + (currentM  * 60) +(currentH  * 3600);
+
+
+}
+
+
+
+const secDay = 24*3600
+var ttc
+var tts
+
+var s,m,h
+s = 0
+m = 0
+h = 0
+
+
+
+function setTtc(){
+    console.log(currentS+ "" + currentM + "" + currentH)
+    tm = Number(currentM* 60) + Number(currentS)
+    
+    ttc = Number(currentS) + Number(currentM * 60) + Number(currentH  * 3600);
+    
+    
+    console.log(tm+ "RESULTADO" )
+
+    
+
+    
+}
+
+function setTts(){
+    tts = Number(saveS + (saveM  * 60) + (saveH  * 3600));
+    
+}
+
+
+function a(){
+    
+    
+
+    setTtc()
+
+    setTts()
+    
+    
+    
+    
+    // 4000 % 3600 =
+    
+    //if  current time 600 %% save 80000  600 - 8000  
+    
+    var tt = 0;
+    
+    //0:10 é menor 23:50
+    // 600 + secDay - 85800
+    
+    
+    tt = ttc - tts
+    console.log("Total :  "+ tt + "=" + ttc +"-"+ tts)
+    
+    
+    
+    
+    
+    
+    
+
+    
+    /*
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    */
+   
+   
+   
+   // fs = arredondar tt/60
+   //161 / 60 = 2,68333
+   // fs = 2
+   // 161- 60 * fs
+   
+   
+   var fs
+   
+   
+   h = Math.floor(tt / 3600)
+   m = Math.floor((tt % 3600) / 60)
+   s = Math.floor((tt%3600) % 60)
+   
+//    s = Math.floor((tt % 3600) % 60)
+
+    s = s < 10  ? "0"+ s : s
+
+    m = m < 10  ? "0"+ m : m
+
+    h = h < 10  ? "0"+ h : h
+    
+    document.getElementById('time').innerHTML= h + ":" + m + ":" + s;            
+    document.getElementById('txt').innerHTML=  h + ":" + m + ":" + s;            
+
+    
+    /*console.log("Tempo inicial : " + saveH + ":"  + saveM + ":" + saveS + " tempo final : " +
+    currentH + ":" + currentM + ":" + currentS + "         diferença "+ _m  + " " + _s)*/
+    
+    
+    document.getElementById('txt3').innerHTML= saveH +":"+saveM +":"+saveS;
+    
+    document.getElementById('txt4').innerHTML= temph +":"+tempm +":"+temps;
+    
+    
+    if(s >= 60 || m >= 60 || h >= 24){
+        tt = tts - ttc;
+        console.log("erro. tempo total : " + tt)
+        alert("erro")
+        
+        
+    }
+    
+    
+    /*
+    console.log(tt + " tt")
+    console.log(h + " h")
+    console.log(m + "m")
+    console.log(s + "s")
+    */
+   
+}
+
+
+function saveTime(){
+    
+    saveS = currentS
+    saveM = currentM;
+    saveH = currentH
+    //   console.log("aaaaaaaaaaaa" + currentH + " " + currentM  + " " + currentS)
+    //   console.log("bbbbbbbbbb" + saveH + " " + saveM  + " " + saveS)
+}
